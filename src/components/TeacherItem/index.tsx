df62ddd85854c72ae678de8724853894ea10ce15
Janaina Oliveira <janainaon1@gmail.com>
2020-08-05 20:28:36 -0300

    Aula02_finalizado

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars1.githubusercontent.com/u/66140932?s=460&u=eae69df15eeeb5684c7eecdaf78b689c56a357a5&v=4" alt="Janaina Oliveira"/>
+        <div>
+          <strong>Janaina Oliveira</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /><br />
+        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 20,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
