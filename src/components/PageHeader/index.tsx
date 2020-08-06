ff7cc614fd9facd3910f7cec47c79b9d1f0bbd3e
Janaina Oliveira <janainaon1@gmail.com>
2020-08-06 17:48:37 -0300

    frontend finalizado

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -23,6 +23,9 @@ const PageHeader: React.FC<PageHeaderProps > = (props) => {
 
     <div className="header-content">
       <strong>{props.title}</strong>
+
+      {props.description && <p>{props.description}</p> }
+
       {props.children }
     </div>
   </header>
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -8,13 +8,25 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+  
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div className="logo-cotainer">
+        <div className="logo-container">
           <img src={logoImg} alt="Proffy logo"/>
           <h2>Sua plataforma de estudos online</h2>
         </div>
@@ -34,7 +46,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
 
